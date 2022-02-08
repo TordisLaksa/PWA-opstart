d125a73b68ae6a5478c23531cad2f736b4bc0e7c
TordisLaksa <88707360+TordisLaksa@users.noreply.github.com>
2022-02-08 14:21:06 +0100

    updates

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+// ServiceWorker registration
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../../sw.js')
+
+        .then(registration => {
+            console.log('sw registered');
+            console.log(registration);
+        }).catch(error => {
+            console.log('sw reg fail');
+            console.log(error);
+        })
+} else {
+    //browser does not support service worker
+    alert('PWA not supported')
+    //do non pwa stuff here
+}
---
+++ sw.js
@@ -0,0 +1,32 @@
+let CACHE_NAME = "my-site-cache";
+
+let urlsToCache = [
+    './',
+    './assets/css/main.css',
+    './assets/css/info.css',
+    './assets/images/silly-your-dog-drawings.jpg',
+    './assets/images/Logo192.png',
+    './assets/images/Logo256.png',
+    './assets/images/Logo512.png',
+    './index.html',
+    './info.html'
+];
+
+//alle elementer til at kabe appen skal caches
+
+self.addEventListener('install', function (event) {
+    //preform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('intercept req: ' + e.request.url);
+    //Caching srategi goes here
+});
